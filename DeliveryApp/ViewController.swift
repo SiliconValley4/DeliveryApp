b1358748d9761acddd26e1827761f715f0ff61e8
Orlando Vargas <Orlandov87@live.com>
2021-10-19 21:55:02 -0400

    Initial Commit

--- DeliveryApp/ViewController.swift
+++ DeliveryApp/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        let button = SVButton(backgroundColor: .blue, title: "Hello")
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 48),
+            button.widthAnchor.constraint(equalToConstant: 100)
+        ])
     }
-
-
 }
-
