a7bdf55555b0d6be528d644b0ebee97ca41c890a
Orlando Vargas <Orlandov87@live.com>
2021-10-21 18:59:30 -0400

    Create LoginViewController.swift

---
+++ DeliveryApp/Screens/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  DeliveryApp
+//
+//  Created by Orlando Vargas on 10/21/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+}
