5872cc0ea21f698387108be5facc6b448aa8efee
Orlando Vargas <Orlandov87@live.com>
2021-10-21 19:15:19 -0400

    Update LoginViewController.swift

--- DeliveryApp/Screens/LoginViewController.swift
+++ DeliveryApp/Screens/LoginViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
     }
-    
-    
 }
