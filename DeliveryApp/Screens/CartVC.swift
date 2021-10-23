27a52e9b053ac398669c38bd894806486120dfe0
Orlando Vargas <Orlandov87@live.com>
2021-10-23 13:31:51 -0400

    Added the rest of the ViewControllers

---
+++ DeliveryApp/Screens/CartVC.swift
@@ -0,0 +1,29 @@
+//
+//  CartVC.swift
+//  DeliveryApp
+//
+//  Created by Orlando Vargas on 10/23/21.
+//
+
+import UIKit
+
+class CartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DeliveryApp/Screens/ProfileVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileVC.swift
+//  DeliveryApp
+//
+//  Created by Orlando Vargas on 10/23/21.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
