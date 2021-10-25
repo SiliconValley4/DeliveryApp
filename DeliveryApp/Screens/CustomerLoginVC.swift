fbf155dc4b2b557d6c4baa62f71dd9b3a88f9b7b
Orlando Vargas <Orlandov87@live.com>
2021-10-24 20:43:51 -0400

    Update CustomerLoginVC.swift

--- DeliveryApp/Screens/CustomerLoginVC.swift
+++ DeliveryApp/Screens/CustomerLoginVC.swift
@@ -16,7 +16,9 @@ class CustomerLoginVC: UIViewController {
     let passwordTextField = SVTextField(placeholder: Constants.Strings.Misc.password)
     let signInButton = SVButton(backgroundColor: Constants.Colors.main, title: Constants.Strings.Button.signIn)
     let cancelButton = SVButton(backgroundColor: .gray, title: Constants.Strings.Button.cancel)
-    let createAccountButton = SVButton(backgroundColor: .clear, title: Constants.Strings.Misc.needAccount)
+    let signInDescription = SVBodyLabel(textAlignment: .center)
+    let createAccountButton = SVButton(backgroundColor: .systemRed, title: Constants.Strings.Misc.needAccount)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +37,16 @@ class CustomerLoginVC: UIViewController {
         view.addSubview(passwordTextField)
         view.addSubview(signInButton)
         view.addSubview(cancelButton)
+        view.addSubview(signInDescription)
+        view.addSubview(createAccountButton)
     }
     
     func setUpButtons() {
         signInButton.addTarget(self, action: #selector(onSubmit), for: .touchUpInside)
         cancelButton.addTarget(self, action: #selector(onCancel), for: .touchUpInside)
+        createAccountButton.addTarget(self, action: #selector(onCreate), for: .touchUpInside)
+        
+        signInDescription.textColor = .black
     }
     
     @objc func onSubmit() {
@@ -52,19 +59,27 @@ class CustomerLoginVC: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @objc func onCreate() {
+        let createAccountView = RegisterVC()
+        
+        present(createAccountView, animated: true, completion: nil)
+    }
+    
     func configureView() {
         appTitle.text = Constants.Strings.Title.customer_login
         welcomeTitle.text = Constants.Strings.Title.welcome_back
+        signInDescription.text = Constants.Strings.Misc.needAccount
+        createAccountButton.titleLabel?.textColor = Constants.Colors.main
         NSLayoutConstraint.activate([
             appTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             appTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
             appTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
             appTitle.heightAnchor.constraint(equalToConstant: view.bounds.height/15),
-            welcomeTitle.topAnchor.constraint(equalTo: appTitle.bottomAnchor),
+            welcomeTitle.topAnchor.constraint(equalTo: appTitle.bottomAnchor, constant: -16),
             welcomeTitle.leadingAnchor.constraint(equalTo: appTitle.leadingAnchor),
             welcomeTitle.trailingAnchor.constraint(equalTo: appTitle.trailingAnchor),
             welcomeTitle.heightAnchor.constraint(equalToConstant: view.bounds.height/17),
-            mainLogo.topAnchor.constraint(equalTo: welcomeTitle.bottomAnchor),
+            mainLogo.topAnchor.constraint(equalTo: welcomeTitle.bottomAnchor, constant: -16),
             mainLogo.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mainLogo.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mainLogo.heightAnchor.constraint(equalTo: view.widthAnchor),
@@ -79,7 +94,18 @@ class CustomerLoginVC: UIViewController {
             signInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 16),
             signInButton.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
             signInButton.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
-            signInButton.heightAnchor.constraint(equalToConstant: 44)
+            signInButton.heightAnchor.constraint(equalToConstant: 56),
+            cancelButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 16),
+            cancelButton.leadingAnchor.constraint(equalTo: signInButton.leadingAnchor),
+            cancelButton.trailingAnchor.constraint(equalTo: signInButton.trailingAnchor),
+            cancelButton.heightAnchor.constraint(equalTo: signInButton.heightAnchor),
+            signInDescription.topAnchor.constraint(equalTo: cancelButton.bottomAnchor, constant: 8),
+            signInDescription.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            signInDescription.widthAnchor.constraint(equalToConstant: view.bounds.width/2),
+            signInDescription.heightAnchor.constraint(equalToConstant: 16),
+            createAccountButton.topAnchor.constraint(equalTo: signInDescription.topAnchor),
+            createAccountButton.leadingAnchor.constraint(equalTo: signInDescription.trailingAnchor, constant: 2),
+            createAccountButton.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 }
