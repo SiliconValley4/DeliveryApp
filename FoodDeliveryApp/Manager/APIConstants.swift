b83f46f7a1a8f7bda0a0505a21f34b72a6985795
Orlando Vargas <Orlandov87@live.com>
2021-11-12 13:33:48 -0500

    Created and Updated APIConstants file

---
+++ FoodDeliveryApp/Manager/APIConstants.swift
@@ -0,0 +1,23 @@
+//
+//  APIConstants.swift
+//  FoodDeliveryApp
+//
+//  Created by Orlando Vargas on 11/12/21.
+//
+
+import Foundation
+
+struct APIConstants {
+    struct URL {
+        static let BASE_URL: String = "https://fooddeliverynowapp.herokuapp.com/"
+    }
+    
+    struct Client {
+        static let ID = "Ij7nN3zWD1VEAtD3Zpv0dyIFzuF4eesM6xHzRHMK"
+        static let SKEY = "M6FL8MLOMwcNrHnRA1iE0zGeA0lj2tc0xEtw7p5xzmjvbyY3pY2hrDAsBuXHi7LG5fxCRRXr8pVjxpfeiQhJYgLKY8HAZfIwADKD7uvqGq7QnEvKlik9iuWcGKhqZ7zF"
+    }
+    
+    struct Stripe {
+        static let PKEY = "pk_test_51HrVG3I0pSVz7qhzEY2QqtUIEExOLgxPUNg6DCif6ioIXwD5bkzGazpkgCr8vxf2CR3ALwgsUCDzArymDdUIZ00E00H73KAHLA"
+    }
+}
