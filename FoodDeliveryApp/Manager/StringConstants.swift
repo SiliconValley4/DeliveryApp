13147b6a92a322e897674b26457086cafefae485
Orlando Vargas <Orlandov87@live.com>
2021-11-12 13:26:29 -0500

    Added StringConstant file

---
+++ FoodDeliveryApp/Manager/StringConstants.swift
@@ -0,0 +1,23 @@
+//
+//  StringConstants.swift
+//  FoodDeliveryApp
+//
+//  Created by Orlando Vargas on 11/12/21.
+//
+
+import Foundation
+
+struct StringConstants {
+    struct Title {
+        
+    }
+    
+    struct Headers {
+        
+    }
+    
+    struct UserType {
+        static let CUSTOMER = "customer"
+        static let DRIVER = "driver"
+    }
+}
