7aa04781312628da338d582043b2bdf9653b3a68
Orlando Vargas <Orlandov87@live.com>
2021-12-04 12:53:01 -0500

    Added flowlayout for single column with padding

--- FoodDeliveryApp/Utilities/UIHelper.swift
+++ FoodDeliveryApp/Utilities/UIHelper.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 struct UIHelper {
+    
+    static func createOneColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let availableWidth = width - (padding*2)
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: availableWidth, height: width/3)
+        
+        return flowLayout
+    }
+    
     static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
         let width = view.bounds.width
         let padding: CGFloat = 12
