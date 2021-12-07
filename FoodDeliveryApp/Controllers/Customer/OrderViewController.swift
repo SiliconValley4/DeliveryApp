33cebfb7f0a4fcefe5d2d9943286394567e84e0a
Fernando <ferg.developer@gmail.com>
2021-12-07 17:59:59 -0500

    updated the design of Customer Order Status, Driver Status map as well as fonts, added labels to name of customer and address

--- FoodDeliveryApp/Controllers/Customer/OrderViewController.swift
+++ FoodDeliveryApp/Controllers/Customer/OrderViewController.swift
@@ -333,5 +333,37 @@ class OrderViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
   //End
+    
+    
+}
 
+extension UIView {
+    
+    @IBInspectable var cornerRadiusV: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidthV: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColorV: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
 }
